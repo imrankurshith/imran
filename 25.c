22247167851b61716c9bd041f39550cf7840f338
imrankurshith <35135606+imrankurshith@users.noreply.github.com>
2018-02-07 15:41:19 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int b[50],n,i,j,a;
+printf("Enter the number : ");
+scanf("%d",&n);
+printf("Enter intergers in array\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+
+
+j=n/2;
+
+				if(j!=0)
+				{
+		printf("\n%d is middle element",b[j]);
+		}
+		}	
+	}
+	return 0;
+}
