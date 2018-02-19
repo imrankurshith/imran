7d0109ddff46fab5bfbf9dbd1f871534f25603f2
imrankurshith <35135606+imrankurshith@users.noreply.github.com>
2018-02-19 16:16:01 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+char s [100];
+printf("enter the string");
+int i,alphabets;
+while(s[i]=!0);
+{
+    if((i<='a'&&i<='z') || (i<='a'&&i<='z'))
+{
+    alphabets++;
+}
+
+}
+}
