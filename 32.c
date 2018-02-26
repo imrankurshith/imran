fcd0ee3b9407ff8f75979ffcfdcf8c9d6976c388
imrankurshith <35135606+imrankurshith@users.noreply.github.com>
2018-02-26 16:02:22 +0530

    Rename 40.c to 32.c

---
+++ 32.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void main()
+{
+int count=0;
+char word[100];
+printf("enter the word:");
+gets[words];
+{
+ if(words==0);
+ word++;
+}
+printf("/n enter the word %d",word);
+return(0);
+}
