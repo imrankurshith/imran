35fa7e5b1d9b17276fcd514a286a09792fd1e817
imrankurshith <35135606+imrankurshith@users.noreply.github.com>
2018-03-02 16:18:27 +0530

    Update 32.c

--- 32.c
+++ 32.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include<string.h>
-void main() {
-	char a[50];
-	int i,count=1;
-	gets(a);
-	for(i=0;a[i]!='\0';i++)
-	{
-		if(a[i] == ' ')
-		count++;	
-	}
-	printf("%d",count);
-	return 0;
+void main()
+{
+   char s[100];
+   int l,count=1,i;
+   scanf("%s",s);
+   l=strlen(s);
+   for(i=0;i<l;i++)
+   {
+       if(s[i]==' ')
+       {
+       count++;
+       printf("%d",count);
+       }
+       else
+       printf("%d",count);
+   }
+    return 0;
 }
 
 
