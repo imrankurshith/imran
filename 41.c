a23b0e1cbc5fdcb958bd92e9f0ddd08fd240aed8
imrankurshith <35135606+imrankurshith@users.noreply.github.com>
2018-03-16 16:58:18 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char a[5];
+    int b,i;
+    printf("enter the name");
+    scanf("%s",a);
+    scanf("%d",&b);
+    for(i=0;i<b;i++)
+    {
+    printf("%s",a);
+    }
+    
+}
